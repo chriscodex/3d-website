cfd1abe01543e8e0b3093e75f19cd8d91933c857
Christian <c.espinoza.code@gmail.com>
2023-08-09 22:41:30 -0500

    feat: locoScroll initialized

---
+++ script.js
@@ -0,0 +1,11 @@
+function locoScroll() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true,
+  });
+
+}

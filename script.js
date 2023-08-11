08599857dcabcddfc9e1fe9f8d75fce33ca318bd
Christian <c.espinoza.code@gmail.com>
2023-08-10 23:01:14 -0500

    feat: scroller proxy configured for main id

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ function locoScroll() {
   // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
   locoScroll.on('scroll', ScrollTrigger.update);
 
-  
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy('#main', {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+  });
 
 }

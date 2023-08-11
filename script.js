ad25950ed08e0f602f6fa07573b7f556af218f84
Christian <c.espinoza.code@gmail.com>
2023-08-10 23:03:11 -0500

    feat: refresh trigger function invokerd

--- script.js
+++ script.js
@@ -34,4 +34,6 @@ function locoScroll() {
   // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
   ScrollTrigger.addEventListener('refresh', () => locoScroll.update());
 
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
 }

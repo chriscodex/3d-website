94861271bb0fdc05bb306068849cd79349043113
Christian <c.espinoza.code@gmail.com>
2023-08-10 23:01:45 -0500

    feat: top 0 & left 0 configured for stroll trigger

--- script.js
+++ script.js
@@ -16,6 +16,14 @@ function locoScroll() {
       return arguments.length
         ? locoScroll.scrollTo(value, 0, 0)
         : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
     },
   });
 
